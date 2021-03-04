f5796cd88bea1c706e1a959f6fe7225dd33f4184
Ian Seng <ianseng@outlook.com>
2021-03-04 16:27:23 -0700

    testing

--- client/server.js
+++ client/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 3000;
-const app = express();
+// const express = require('express');
+// const favicon = require('express-favicon');
+// const path = require('path');
+// const port = process.env.PORT || 3000;
+// const app = express();
 
-// Derrived from https://medium.com/jeremy-gottfrieds-tech-blog/tutorial-how-to-deploy-a-production-react-app-to-heroku-c4831dfcfa08
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+// // Derrived from https://medium.com/jeremy-gottfrieds-tech-blog/tutorial-how-to-deploy-a-production-react-app-to-heroku-c4831dfcfa08
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// // the __dirname is the current directory from where the script is running
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-console.log("Server is up")
+// console.log("Server is up")
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+// app.listen(port);
