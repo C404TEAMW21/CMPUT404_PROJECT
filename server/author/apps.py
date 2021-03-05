7ddcfe0d3f51583ff59810c588e6f59eb7b263b5
Ian Seng <ianseng@outlook.com>
2021-03-04 22:08:49 -0700

    wireframes

--- server/author/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class AuthorsConfig(AppConfig):
-    name = 'author'
--- server/inbox/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from inbox import views
-
-app_name = 'inbox'
-
-urlpatterns = [
-    path('<uuid:author_id>/inbox/', views.InboxView.as_view(), name='inbox')
-]
