b1796958c8369f88e766540b5ffa8cce07e1e8be
Amy Xiang <yangyi1@ualberta.ca>
2021-04-10 13:51:09 -0600

    feat: fix comment count

--- client/src/components/Post/PostList.js
+++ client/src/components/Post/PostList.js
@@ -16,7 +16,6 @@ const PostList = ({ posts, handleDeletePost }) => {
       author,
       published,
       visibility,
-      count,
     } = post;
 
     if (post.type === "github") {
@@ -37,7 +36,6 @@ const PostList = ({ posts, handleDeletePost }) => {
         author={author}
         published={moment(published).format("MMMM Do YYYY, h:mm:ss a")}
         visibility={visibility}
-        commentCount={count}
         handleDeletePost={handleDeletePost}
       />
     );
